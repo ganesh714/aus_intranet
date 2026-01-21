f9fae053dfad268e6de183fb01ab408dff253cbd
ganesh714 <evvganesh1@gmail.com>
2026-01-21 07:57:46 +0530

    refactor(backend): move announcement upload logic to controller

--- backend/routes/announcementRoutes.js
+++ backend/routes/announcementRoutes.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const announcementController = require('../controllers/announcementController');
 
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
+
 router.get('/get-announcements', announcementController.getAnnouncements);
+router.post('/add-announcement', upload.single('file'), announcementController.addAnnouncement);
 
 module.exports = router;
