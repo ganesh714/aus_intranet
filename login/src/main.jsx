c5c7a5a89b67ff01b12c39be9f7140baadd4843b
ganesh714 <evvganesh1@gmail.com>
2026-02-06 22:23:45 +0530

    updated auth

--- login/src/main.jsx
+++ login/src/main.jsx
@@ -3,6 +3,33 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
+import axios from 'axios';
+
+// [NEW] Axios Interceptors
+axios.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Clear session and redirect to login
+      sessionStorage.clear();
+      window.location.href = '/LoginForm';
+    }
+    return Promise.reject(error);
+  }
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
