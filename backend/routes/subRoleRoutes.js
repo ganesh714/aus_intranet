e97084a9dfb8b48e7edd7bb5e3d2eb8fa9a319e4
Surya <surya@gmail.com>
2026-02-26 13:34:24 +0530

    solved

--- backend/routes/subRoleRoutes.js
+++ backend/routes/subRoleRoutes.js
@@ -4,7 +4,7 @@ const subRoleController = require('../controllers/subRoleController');
 
 router.get('/all-subroles', subRoleController.getAllSubRoles);
 router.get('/subroles/:role', subRoleController.getSubRolesByRole);
-router.get('/subroles/details/:idgit branch', subRoleController.getSubroledetails);
+router.get('/subroles/details/:id', subRoleController.getSubroledetails);
 router.post('/add-subrole', subRoleController.createSubRole);
 router.delete('/delete-subrole/:id', subRoleController.deleteSubRole);
 
